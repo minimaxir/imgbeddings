a88f7b9e8be7d2ba56ea765d651a658bc6d6d899
Max Woolf <max@minimaxir.com>
2022-03-19 16:49:35 -0700

    scaffolding

---
+++ imgbeddings.py
@@ -0,0 +1,63 @@
+import logging
+import torch
+from dataclasses import dataclass, field
+
+from transformers import CLIPProcessor, CLIPVisionModel
+
+logger = logging.getLogger("imgbeddings")
+logger.setLevel(logging.INFO)
+
+
+@dataclass
+class imgbeddings:
+    patch_size: int = 32
+    model: CLIPVisionModel = field(init=False)
+    processor: CLIPProcessor = field(init=False)
+
+    r_brightness: float = 0.2
+    r_contrast: float = 0.2
+    r_saturation: float = 0.2
+    r_hue: float = 0.01
+    r_degrees: int = 15
+    r_scale: float = 0.10
+
+    def __post_init__(self):
+        patch_values = [14, 16, 32]
+        assert self.patch_size in patch_values, f"patch_size must be in {patch_values}."
+
+        self.model = CLIPVisionModel.from_pretrained(
+            f"openai/clip-vit-base-patch{self.patch_size}"
+        )
+        self.processor = CLIPProcessor.from_pretrained(
+            f"openai/clip-vit-base-patch{self.patch_size}"
+        )
+
+        self.model.eval()
+
+    def create_embeddings(self, inputs, num_layers=3):
+        image_inputs = self.processor(images=inputs, return_tensors="pt")
+
+        with torch.no_grad():
+            outputs = self.model(
+                **image_inputs, output_attentions=True, output_hidden_states=True
+            )
+
+        hidden_states = torch.sum(
+            torch.stack([outputs.hidden_states[i] for i in range(-num_layers, 0)]), 0
+        )
+
+        attentions = torch.stack([outputs.attentions[i] for i in range(-num_layers, 0)])
+        # switch dimensions so batch dimension is first
+        attentions = torch.transpose(attentions, 1, 0)
+
+        attentions_reduced = torch.mean(attentions, (1, 2, 3))
+        attentions_reweighted = attentions_reduced
+
+        # the first value corresponds to the class token which is irrelevant
+        attentions_reweighted[:, 0] = 0.0
+        attentions_reweighted = attentions_reweighted / torch.sum(
+            attentions_reweighted, 1
+        )
+
+        embeddings = hidden_states * attentions_reweighted.unsqueeze(2)
+        return embeddings.sum(1)
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="imgbeddings",
+    packages=["imgbeddings"],  # this must be the same as the name above
+    version="0.1.0",
+    description="A robust Python tool for text-based AI training and generation using GPT-2.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Max Woolf",
+    author_email="max@minimaxir.com",
+    url="https://github.com/minimaxir/imgeddings",
+    keywords=["gpt-2", "gpt2", "text generation", "ai"],
+    classifiers=[],
+    license="MIT",
+    # entry_points={"console_scripts": ["aitextgen=aitextgen.cli:aitextgen_cli"]},
+    python_requires=">=3.7",
+    include_package_data=True,
+    install_requires=[
+        "transformers>=4.17.0",
+        # "fire>=0.3.0",
+        "torch>=1.11",
+    ],
+)
